6e9ceb7cde1e99c1f3fac5dd4af8fb1590501364
Boris Okunskiy <boris@okunskiy.name>
2023-02-28 15:30:03 +0300

    chore: put secrets to MongoDb.connect

--- src/test/MongoDomain.test.ts
+++ src/test/MongoDomain.test.ts
@@ -14,6 +14,7 @@ describe('MongoDomain', () => {
             });
             await runtime.Mongo.connect({
                 url: runtime.testMongoDb.MONGO_URL,
+                secret: '',
             });
             const { document } = await runtime.Mongo.findOne({
                 collection: 'users',
